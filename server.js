a55182ede6dd3896687b93c98b4f6a61e383759d
Gunnar Ulatowski <gunnarulatowski@outlook.com>
2021-07-27 22:33:58 -0400

    deploying

---
+++ server.js
@@ -0,0 +1,38 @@
+//dependencies
+
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+const logger = require("morgan");
+
+//port
+
+const PORT = process.env.PORT || 3000;
+
+//--//
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+//creates db when server is started up in terminal
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
+app.listen(PORT, () => {
+  console.log(`Application is running on http://localhost:${PORT}`);
+});
